15ec1d7fa7d59c6e3e18395d4f8c81b808d514cf
esecretaria <ejsecretaria@gmail.com>
2024-04-08 21:50:42 -0600

    updated live backend baseUrl

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+//const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://status-be.onrender.com/api/v1";
 
 export default BASE_URL;
